0cab052e126269b76a75bcbab70fd5f862a3e1d1
prathapkumar24 <prathapkumar240@gmail.com>
2025-11-24 16:54:22 +0530

    test change

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use('/auth', authRoutes);
 
 // Root route
 app.get('/', (req, res) => {
-  res.json({ message: 'Welcome to Movie DB API' });
+  res.json({ message: 'Welcome to Movie DB API - AWS' });
 });
 
 // Error handling middleware
